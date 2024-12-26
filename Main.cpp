8a73cd66ecba69146b383503dcc872f316f1d5dc
Riccardo Foglio <riccardo.fogliopara@gmail.com>
2024-12-26 18:17:49 +0100

    test

--- Main.cpp
+++ Main.cpp
@@ -13,7 +13,6 @@
 
 #include <glm/gtx/intersect.hpp> // For ray-box intersection
 
-
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
